b1ca8f75f0403910c42ac5dad23d06631ef629d2
SOUMYAJIT MANDAL <95221573+Soumyajit-Mandal@users.noreply.github.com>
2024-05-29 16:41:53 +0530

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -19,13 +19,13 @@ select * from Sales.Store order by Name
 select * from Sales.Store where Name like 'A%'
 
 --7. List of customers who ever placed an order
---wrong
+
 select * from Sales.Store
 
 --8. list of Customers who live in London and have bought chai
 
 --11. Details of first order of the system
---check
+
 select * from sales.SalesOrderHeader GROUP BY MIN(OrderDate);
 
 
@@ -36,6 +36,7 @@ select * from sales.SalesOrderHeader where SubTotal=(select MAX(SubTotal) from s
 
 --13. For each order get the OrderID and Average quantity of items in that order
 select SalesOrderID , AVG(OrderQty) from sales.SalesOrderDetail GROUP BY SalesOrderID;
+
 --14. For each order get the orderID, minimum quantity and maximum quantity for that order
 
 select ProductID,MaxOrderQty,MinOrderQty from Purchasing.ProductVendor ;
@@ -47,24 +48,32 @@ select ProductID, OnorderQty from Purchasing.ProductVendor where OnOrderQty > 30
 
 --17. list of all orders placed on or after 1996/12/31
 select * from Purchasing.PurchaseOrderHeader where OrderDate >= '1996-12-31';
+
 --18. list of all orders shipped to Canada
 select * from Person.Address where city = 'Canada';
+
 --19. list of all orders with order total > 200
 select SalesOrderID, OrderQty from Sales.SalesOrderDetail where OrderQty > 200;
 
 --20. List of countries and sales made in each country
+select * from [Sales].[SalesOrderHeader]
 
 --21. List of Customer ContactName and number of orders they placed
-select * from 
+select * from Sales.SalesOrderDetail
+
 --22. List of customer contactnames who have placed more than 3 orders
+select SalesOrderID , OrderQty from Sales.SalesOrderDetail where OrderQty>3
 
 --23. List of discontinued products which were ordered between 1/1/1997 and 1/1/1998
+
 --24. List of employee firsname, lastName, superviser FirstName, LastName
+select FirstName, LastName  from Person.Person
+
 --25. List of Employees id and total sale condcuted by employee
 select SalesPersonID,SubTotal from Sales.SalesOrderHeader;
---26. list of employees whose FirstName contains character a
-select FirstName from Person.Person where FirstName  'a'
 
+--26. list of employees whose FirstName contains character a
+select FirstName from Person.Person where FirstName like '%a%'
 
 --27. List of managers who have more than four people reporting to them.
 
@@ -75,8 +84,9 @@ select BusinessEntityID,Name from Sales.Store
 select * from sales.SalesOrderHeader where SubTotal=(select MAX(SubTotal) from sales.SalesOrderHeader)
 
 --30. List of orders placed by customers who do not have a Fax number
-
+select * from Person.PersonPhone;
 --31. List of Postal codes where the product Tofu was shipped
+
 --32. List of product Names that were shipped to France
 select * from Person.Address where city = 'France';
 
@@ -98,8 +108,12 @@ select * from sales.SalesOrderHeader where SubTotal=(select MAX(SubTotal) from s
 
 --39. Product name and total revenue from that product
 --40. Supplierid and number of products offered
+select SalesOrderID, OrderQty from Sales.SalesOrderDetail
 
 --41. Top ten customers based on their business
 select top 10 * from sales.SalesOrderDetail ORDER BY OrderQty DESC;
 
 --42. What is the total revenue of the company.
+select SalesOrderID,  sum(SubTotal)
+from Sales.SalesOrderHeader
+group by SalesOrderID with rollup
